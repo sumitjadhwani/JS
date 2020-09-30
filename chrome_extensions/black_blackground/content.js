9e06b6b0ba06b8da74a651f38c4a70088450b3e6
sumitjadhwani <42548520+sumitjadhwani@users.noreply.github.com>
2020-10-01 00:06:27 +0530

    Added comments

--- chrome_extensions/black_blackground/content.js
+++ chrome_extensions/black_blackground/content.js
@@ -1,4 +1,8 @@
+//Storing document title in variable
 var title=document.title;
+
+//Printing title on console
 console.log("Welcome to ",title);
 
+//Changing the background to black using HTML DOM
 document.body.style.background="rgb(0,0,0)";
